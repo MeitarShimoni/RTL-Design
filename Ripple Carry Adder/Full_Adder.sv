d6fc8f3eaa466468b8df974e8c69733d415ef420
Meitar Shimoni <141142200+MeitarShimoni@users.noreply.github.com>
2025-10-27 18:00:57 +0200

    Create Full_Adder.sv

---
+++ Ripple Carry Adder/Full_Adder.sv
@@ -0,0 +1,18 @@
+
+
+
+`timescale 1ns/1ps
+
+module Full_Adder(
+    input A,
+    input B,
+    input Cin,
+    output S,
+    output Cout
+    );
+
+ assign S = ((A ^ B) ^ Cin);
+ assign Cout = ((A^B)&Cin)|(A&B);
+
+
+endmodule
